62dc4157661a0c2af69aa701b9e87911b5159786
Ivan Shi <ivan@shiryaeff.ru>
2022-10-08 11:39:04 +0300

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,15 +1,15 @@
-#define main_time 1000
-
-#define up 50
-#define down 100
-#define back 200
-#define forward 300
-#define left 400
-#define right 500
+#define main_time 100
+#define error_rate 1
+#define up 3
+#define down 4
+#define back 5
+#define forward 6
+#define left 7
+#define right 8
 
 
 volatile int counter = 0;
-int analogPin = A2;
+int analogPin = A0;
 
 void setup() {
   Serial.begin(9600);
@@ -28,7 +28,7 @@ void loop() {
 
   while (i < main_time){
 
-    if (analogRead(analogPin) > 500){
+    if (analogRead(analogPin) > 1000){
       digitalWrite(5, LOW);
       delay(5);
       i+=1;
@@ -40,25 +40,25 @@ void loop() {
     }
   }
 
-  if(counter <= forward + 25 && counter >= forward - 25){
+  if(counter <= forward + error_rate && counter >= forward - error_rate){
     Serial.println("f");
   }
-  if(counter <= back + 25 && counter >= back - 25){
+  if(counter <= back + error_rate && counter >= back - error_rate){
     Serial.println("b");
   }
-  if(counter <= left + 25 && counter >= left - 25){
+  if(counter <= left + error_rate && counter >= left - error_rate){
     Serial.println("l");
   }
-  if(counter <= right + 25 && counter >= right - 25){
+  if(counter <= right + error_rate && counter >= right - error_rate){
     Serial.println("r");
   }
-  if(counter <= up + 25 && counter >= up - 25){
+  if(counter <= up + error_rate && counter >= up - error_rate){
     Serial.println("u");
   }
-  if(counter <= down + 25 && counter >= down - 25){
+  if(counter <= down + error_rate && counter >= down - error_rate){
     Serial.println("d");
   }
-  //Serial.println(counter); 
+  //Serial.print(counter); 
   counter = 0;
   //Serial.print(" "); 
   //Serial.println(analogRead(analogPin));  
